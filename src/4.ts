aead22a8d93503f1091808f162ae3c993c830d56
volodymyr <v.pohoretskyi@gmail.com>
2025-06-22 17:35:48 +0200

    HW

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,56 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor(signature?: number) {
+    this.signature = signature ?? Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+  clone(): Key {
+    return new Key(this.signature);
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
